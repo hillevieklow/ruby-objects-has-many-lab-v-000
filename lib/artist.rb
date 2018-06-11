323763223a209c8b7c31682c5757e932e9f4731a
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-11 16:52:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,12 @@
 class Artist
+
   attr_accessor :name :songs
+
   def initialize(name)
     @name = name
     @songs = []
   end
 
-  def songs
-    @songs
-  end
 
   def add_song(song)
     @songs << song
